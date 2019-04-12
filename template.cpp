10e586fd5924d3f13f5d684be54f6e8b647f31c3
longtime1116 <longtime1116@gmail.com>
2019-04-13 00:42:28 +0900

    update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,8 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define REP(i,n) for(int i=0;i<n;i++)
+typedef long long int lli;
+typedef pair<int, int> P;
+
 int main() {
     int a;
+    lli hoge;
     cin >> a;
-    cout << << endl;
+    cout << a << endl;
 }
