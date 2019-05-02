a48e541aafe68e652a52ae0451f84e60c9bbe6b5
longtime1116 <longtime1116@gmail.com>
2019-05-02 23:18:48 +0900

    abc032 dp 部分完成

--- abc032/d.cpp
+++ abc032/d.cpp
@@ -22,7 +22,7 @@ int main() {
     }
     if (max_w <= 1000) {
         // w が 1 <= w <= 1000 を満たすときは、重さの総和は MAX 200000 なので、dp でいける
-        // dp[i][j]: i 個目まで使える条件のもと 重さの総和を j 以下に抑えるときの最大の価値
+        // dp[i][j]: i 個目まで使える条件のもと重さの総和を j にするときの最大の価値
         bool pos = false;
         for(int i=0;i<N;i++){
             REP(j, W+1) {
@@ -37,6 +37,28 @@ int main() {
     }
     else if (max_v <= 1000) {
         // v が 1 <= v <= 1000 を満たすときは、価値の総和は 200000 なので、dp でいける
+        // dp[i][j]: i 個目まで使える条件のもと価値の総和を j にするときの最小の重さ
+        REP(i, 2) {
+            REP(j, 200001) {
+                if (j == 0 )
+                    dp[i][j] = 0;
+                else
+                    dp[i][j] = 1000000001;
+            }
+        }
+        bool pos = false;
+        for(int i=0;i<N;i++){
+            for(lli j=0;j<200001;j++) {
+                if (j-v[i]>=0)
+                    dp[!pos][j] = min(dp[pos][j], (dp[pos][j-v[i]] + w[i]));
+                else
+                    dp[!pos][j] = dp[pos][j];
+
+                if (dp[!pos][j] <= W)
+                    ans = max(ans, j);
+            }
+            pos = !pos;
+        }
     }
     else {
         // N <= 30 の条件のみなので、dp はきつい
