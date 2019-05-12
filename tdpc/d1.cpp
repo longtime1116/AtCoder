836c0c9927af1e1ee609776e67e5b258972d3205
longtime1116 <longtime1116@gmail.com>
2019-05-12 15:04:18 +0900

    d.cpp is the best solution

--- tdpc/d1.cpp
+++ tdpc/d1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 typedef long long int lli;
 typedef pair<int, int> P;
 
-map<lli, int> fac;
+map<lli, lli> fac;
 
 // dp[i][j][k][l]: i個目のサイコロを振った時点で2,3,5がそれぞれj,k,l個使われている確率
 double dp[101][63][63][63];
@@ -34,31 +34,38 @@ int main() {
 
     dp[0][0][0][0] = 1.0;
     REP(i, 0, n) {
-        REP(j, 0, fac[2]+1) {
-            REP(k, 0, fac[3]+1) {
-                REP(l, 0, fac[5]+1) {
+        REP(j, 0, 61) {
+            REP(k, 0, 61) {
+                REP(l, 0, 61) {
                     // 1
                     dp[i+1][j][k][l] += dp[i][j][k][l] * 1 / 6.0;
 
                     // 2
-                    dp[i+1][min(j+1, fac[2])][k][l] += dp[i][j][k][l] * 1 / 6.0;
+                    dp[i+1][min(j+1, 60)][k][l] += dp[i][j][k][l] * 1 / 6.0;
 
                     // 3
-                    dp[i+1][j][min(k+1, fac[3])][l] += dp[i][j][k][l] * 1 / 6.0;
+                    dp[i+1][j][min(k+1, 60)][l] += dp[i][j][k][l] * 1 / 6.0;
 
                     // 4
-                    dp[i+1][min(j+2, fac[2])][k][l] += dp[i][j][k][l] * 1 / 6.0;
+                    dp[i+1][min(j+2, 60)][k][l] += dp[i][j][k][l] * 1 / 6.0;
 
                     // 5
-                    dp[i+1][j][k][min(l+1, fac[5])] += dp[i][j][k][l] * 1 / 6.0;
+                    dp[i+1][j][k][min(l+1, 60)] += dp[i][j][k][l] * 1 / 6.0;
 
                     // 6
-                    dp[i+1][min(j+1, fac[2])][min(k+1, fac[3])][l] += dp[i][j][k][l] * 1 / 6.0;
+                    dp[i+1][min(j+1, 60)][min(k+1, 60)][l] += dp[i][j][k][l] * 1 / 6.0;
                 }
             }
         }
     }
 
-    double ans = dp[n][fac[2]][fac[3]][fac[5]];
+    double ans = 0;
+    REP(j, fac[2], 61) {
+        REP(k, fac[3], 61) {
+            REP(l, fac[5], 61) {
+                ans += dp[n][j][k][l];
+            }
+        }
+    }
     cout << ans << endl;
 }
