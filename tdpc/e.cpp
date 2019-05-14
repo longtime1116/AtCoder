ac927d5abbd8865ddf1d568f03f9f1837c3ae0c0
longtime1116 <longtime1116@gmail.com>
2019-05-15 01:02:30 +0900

    tdpc e

--- tdpc/e.cpp
+++ tdpc/e.cpp
@@ -8,31 +8,43 @@ typedef pair<int, int> P;
 
 // dp[i][j][k]
 // 左から i 桁(1<=i<=n.length)までの和を D で割った余がj になる個数
-// ただし、N よりも大きいとダメなので、k にそれを入れる。
-int dp[10002][100];
+// ただし、N よりも大きいとダメなので、Nよりも大きい可能性があるケースは別で処理する
+lli dp[10002][100][2];
+lli mod = 1000000007;
 int main() {
+    bool safe = true;
     int d;
     string n;
     cin >> d >> n;
     lli ans = 0;
-    dp[0][0] = 1;
+    dp[0][0][0] = 1;
     for(int i=1; i<=n.length();i++) {
-        int num = n[i-1] - '0';
-        for (int j=0; j < 10; j++) {
+        for (int j=0; j < d; j++) {
+            int num = static_cast<int>(n[i-1] - '0');
+            // danger
+            for (int k=0; k < num; k++) {
+                dp[i][(j+k)%d][safe] = (dp[i][(j+k)%d][safe] + dp[i-1][j][!safe]) % mod;
+            }
+            dp[i][(j+num)%d][!safe] = (dp[i][(j+num)%d][!safe] + dp[i-1][j][!safe]) % mod;
+
+            // safe
             for (int k=0; k < 10; k++) {
-                int target = (j + k) % d;
-                cout << "i: " << i;
-                cout << "target: " << target<<endl;
-                dp[i][target] += dp[i-1][j];
+                dp[i][(j+k)%d][safe] = (dp[i][(j+k)%d][safe] + dp[i-1][j][safe]) % mod;
             }
         }
     }
-    for(int i=1; i<=n.length();i++) {
-        for (int j=0; j < d; j++) {
-            cout << "dp[" <<  i << "]"<< "[" << j << "]: " << dp[i][j] << endl;
-        }
-    }
+    //for(int i=0; i<=n.length();i++) {
+    //    for (int j=0; j < d; j++) {
+    //        cout << "dp[" <<  i << "]"<< "[" << j << "][0]: " << dp[i][j][0] << endl;
+    //    }
+    //}
+    //cout << endl;
+    //for(int i=0; i<=n.length();i++) {
+    //    for (int j=0; j < d; j++) {
+    //        cout << "dp[" <<  i << "]"<< "[" << j << "][1]: " << dp[i][j][1] << endl;
+    //    }
+    //}
 
 
-    cout << ans << endl;
+    cout << (dp[n.length()][0][0] + dp[n.length()][0][1] - 1) % mod << endl;
 }
