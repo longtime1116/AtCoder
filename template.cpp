7ef0134a15ecf7ebf9f22a4cb5b0d1534fc241e9
longtime1116 <longtime1116@gmail.com>
2019-05-26 16:55:46 +0900

    update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,13 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define REP(i,init, n) for(int i=init;i<n;i++)
-#define REPE(i,init, n) for(int i=init;i<=n;i++)
+#define REP(i,init, n) for(lli i=init;i<n;i++)
+#define REPE(i,init, n) for(lli i=init;i<=n;i++)
+#define ARRAY_LENGTH(array) (sizeof(array) / sizeof(array[0]))
+
 typedef long long int lli;
-typedef pair<int, int> P;
+typedef pair<lli, lli> P;
 
 int main() {
-    int n;
-    cin >> n;
-    cout << a << endl;
 }
