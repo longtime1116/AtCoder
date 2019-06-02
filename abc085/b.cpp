dfb82d18478658f5cb92eb04d0fb7b35a194fb1e
longtime1116 <longtime1116@gmail.com>
2019-06-02 18:56:34 +0900

    abc085 b

---
+++ abc085/b.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define REP(i,init, n) for(lli i=init;i<n;i++)
+#define REPE(i,init, n) for(lli i=init;i<=n;i++)
+#define REPIT(it,container) for(auto it = container.begin(); it != container.end(); it++)
+#define REPIT_R(it,container) for(auto it = container.rbegin(); it != container.rend(); it++)
+
+#define ARRAY_LENGTH(array) (sizeof(array) / sizeof(array[0]))
+
+typedef long long int lli;
+typedef pair<lli, lli> P;
+
+int main() {
+    lli n;
+    cin >> n;
+    set<lli> s;
+    REP(i, 0, n) {
+        lli d;
+        cin >> d;
+        s.insert(d);
+    }
+    cout << s.size() << endl;
+}
