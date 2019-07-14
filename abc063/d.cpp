16e7834aee786f19c70d23e37a7871013ee2594e
longtime1116 <longtime1116@gmail.com>
2019-07-14 20:42:58 +0900

    abc063 d tle

---
+++ abc063/d.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// iterator
+#define REP(i,init, n) for(lli i=init;i<n;i++)
+#define REPE(i,init, n) for(lli i=init;i<=n;i++)
+#define REPIT(it,container) for(auto it = container.begin(); it != container.end(); it++)
+#define REPIT_R(it,container) for(auto it = container.rbegin(); it != container.rend(); it++)
+
+// input
+#define cin1(x)             cin >> x
+#define cin2(x, y)          cin >> x >> y
+#define cin3(x, y, z)       cin >> x >> y >> z
+#define ncin1(n, x)         REP(i, 0, n) {cin1(x[i]);}
+#define ncin2(n, x, y)      REP(i, 0, n) {cin2(x[i], y[i]);}
+#define ncin3(n, x, y, z)   REP(i, 0, n) {cin3(x[i], y[i], z[i]);}
+
+#define ARRAY_LENGTH(array) (sizeof(array) / sizeof(array[0]))
+
+typedef long long int lli;
+typedef pair<lli, lli> P;
+
+int main() {
+    lli n, a, b;
+    cin3(n, a, b);
+    lli h[n];
+    priority_queue<lli> que;
+    REP(i, 0, n) {
+        lli h;
+        cin >> h;
+        que.push(h);
+    }
+    lli x = 0;
+    lli ans = 0;
+    while (true) {
+        lli top = que.top();
+        if (top <= x)
+            break;
+        que.pop();
+        que.push(top - a + b);
+        x += b;
+        ans++;
+    }
+    cout << ans << endl;
+}
