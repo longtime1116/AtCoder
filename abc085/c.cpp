e1476effda91af759dc36bae16ad80ccda83b348
longtime1116 <longtime1116@gmail.com>
2019-08-05 22:16:47 +0900

    abc085 a c

---
+++ abc085/c.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// iterator
+#define REP(i,init, n) for(lli i=init;i<n;i++)
+#define REPE(i,init, n) for(lli i=init;i<=n;i++)
+#define REPIT(it,container) for(auto it = container.begin(); it != container.end(); it++)
+#define REPIT_R(it,container) for(auto it = container.rbegin(); it != container.rend(); it++)
+
+// input
+#define cin1(x)             cin >> x
+#define cin2(x, y)          cin >> x >> y
+#define cin3(x, y, z)       cin >> x >> y >> z
+#define ncin1(n, x)         REP(i, 0, n) {cin1(x[i]);}
+#define ncin2(n, x, y)      REP(i, 0, n) {cin2(x[i], y[i]);}
+#define ncin3(n, x, y, z)   REP(i, 0, n) {cin3(x[i], y[i], z[i]);}
+
+// output
+#define cout1(x)         cout << #x << ": " << x << endl;
+#define ncout1(n, x)     REP(i, 0, n) {cout << #x << "[" << i << "]: "<< x[i] << endl;}
+
+// sort
+#define sort_r(x, y)        sort(x, y, greater<lli>()); // 降順(5,4,3,,,)
+
+#define ARRAY_LENGTH(array) (sizeof(array) / sizeof(array[0]))
+
+typedef long long int lli;
+typedef pair<lli, lli> P;
+
+int main() {
+    lli n, y;
+    cin2(n, y);
+    REPE(i, 0, n) {
+        REPE(j, 0, n) {
+            lli k = n - i - j;
+            if (k < 0)
+                continue;
+            if (i * 10000 + j * 5000 + k * 1000 == y) {
+                cout << i << " " << j << " " << k << endl;
+                return 0;
+            }
+        }
+    }
+    cout << "-1 -1 -1" << endl;
+}
