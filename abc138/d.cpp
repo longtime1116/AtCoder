70036b2a4bc7c6679462854e1d4aa4faf3ef4160
longtime1116 <longtime1116@gmail.com>
2019-08-19 10:26:44 +0900

    abc138 a b c d

---
+++ abc138/d.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// iterator
+#define REP(i,init, n) for(lli i=init;i<n;i++)
+#define REPE(i,init, n) for(lli i=init;i<=n;i++)
+#define REPIT(it,container) for(auto it = container.begin(); it != container.end(); it++)
+#define REPIT_R(it,container) for(auto it = container.rbegin(); it != container.rend(); it++)
+
+// input
+#define cin1(x)             cin >> x
+#define cin2(x, y)          cin >> x >> y
+#define cin3(x, y, z)       cin >> x >> y >> z
+#define ncin1(n, x)         REP(i, 0, n) {cin1(x[i]);}
+#define ncin2(n, x, y)      REP(i, 0, n) {cin2(x[i], y[i]);}
+#define ncin3(n, x, y, z)   REP(i, 0, n) {cin3(x[i], y[i], z[i]);}
+
+// output
+#define cout1(x)         cout << #x << ": " << x << endl;
+#define ncout1(n, x)     REP(i, 0, n) {cout << #x << "[" << i << "]: "<< x[i] << endl;}
+
+// sort
+#define sort_r(x, y)        sort(x, y, greater<lli>()); // 降順(5,4,3,,,)
+
+#define ARRAY_LENGTH(array) (sizeof(array) / sizeof(array[0]))
+
+typedef long long int lli;
+typedef pair<lli, lli> P;
+
+
+vector<lli> G[200001];
+lli cost[200001];
+lli ans[200001];
+
+
+void rec(lli cur, lli cur_cost) {
+    ans[cur] = cur_cost;
+    REP(i, 0, G[cur].size()) {
+        rec(G[cur][i], cur_cost + cost[G[cur][i]]);
+    }
+}
+
+int main() {
+    lli n, q;
+    cin2(n, q);
+
+    REP(i, 0, n-1) {
+        lli a, b;
+        cin >> a >> b;
+        G[a].push_back(b);
+    }
+    lli p[q], x[q];
+    REP(i, 0, q) {
+        cin >> p[i] >> x[i];
+        cost[p[i]] += x[i];
+    }
+
+
+
+
+    rec(1, cost[1]);
+    REPE(i, 1, n) {
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+
+
+}
