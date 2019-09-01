5bebfb17fd89b4652ffcd49b344bb06021515972
longtime1116 <longtime1116@gmail.com>
2019-09-01 11:36:47 +0900

    arc005 c

--- arc005/c.cpp
+++ arc005/c.cpp
@@ -17,6 +17,8 @@ using namespace std;
 
 // output
 #define cout1(x)         cout << #x << ": " << x << endl;
+#define cout2(x, y)      cout << #x << ": " << x << ", " << #y << ": " << y << endl;
+#define cout3(x, y, z)   cout << #x << ": " << x << ", " << #y << ": " << y << ", " << #z << ": " << z << endl;;
 #define ncout1(n, x)     REP(i, 0, n) {cout << #x << "[" << i << "]: "<< x[i] << endl;}
 
 // sort
@@ -26,71 +28,12 @@ using namespace std;
 
 typedef long long int lli;
 typedef pair<lli, lli> P;
+typedef tuple<lli, lli, lli> tup;
 
 lli h, w;
 string s[501];
-bool done[501][501];
+bool done[501][501][3];
 
-bool rec(lli r, lli c, lli left) {
-    if (done[r][c])
-        return false;
-
-    if (left == -1)
-        return false;
-
-    if (s[r][c] == 'g')
-        return true;
-
-    //cout << "r: " << r << ", c: " << c << ", left: " << left << endl;
-    done[r][c] = true;
-
-    // left
-    if (c > 0) {
-        if (s[r][c-1] == '#') {
-            if (rec(r, c-1, left-1))
-                return true;
-        }
-        else {
-            if (rec(r, c-1, left))
-                return true;
-        }
-    }
-    // right
-    if (c < w-1) {
-        if (s[r][c+1] == '#') {
-            if (rec(r, c+1, left-1))
-                return true;
-        }
-        else {
-            if (rec(r, c+1, left))
-                return true;
-        }
-    }
-    // up
-    if (r < h-1) {
-        if (s[r+1][c] == '#') {
-            if (rec(r+1, c, left-1))
-                return true;
-        }
-        else {
-            if (rec(r+1, c, left))
-                return true;
-        }
-    }
-    // down
-    if (r > 0) {
-        if (s[r-1][c] == '#') {
-            if (rec(r-1, c, left-1))
-                return true;
-        }
-        else {
-            if (rec(r-1, c, left))
-                return true;
-        }
-    }
-    done[r][c] = false;
-    return false;
-}
 
 int main() {
     cin2(h, w);
@@ -99,16 +42,58 @@ int main() {
         cin >> s[i];
     }
 
+    lli si, sj;
+    lli gi, gj;
+    si = sj = gi = gj = -1;
     REP(i, 0, h) {
         REP(j, 0, w) {
             if (s[i][j] == 's') {
-                if (rec(i, j, 2))
-                    cout << "YES" << endl;
-                else
-                    cout << "NO" << endl;
-                return 0;
+                si = i;
+                sj = j;
+            }
+            if (s[i][j] == 'g') {
+                gi = i;
+                gj = j;
             }
         }
     }
+
+    queue<tup> que;
+    que.push(tup(si, sj, 0));
+    lli move_r[] = {-1, 1, 0, 0};
+    lli move_c[] = {0, 0, -1, 1};
+    while (!que.empty()) {
+        lli r, c, x;
+        tie(r, c, x) = que.front();
+        que.pop();
+        if (x > 2)
+            continue;
+        bool is_done = false;
+        REPE(i, 0, x) {
+            if (done[r][c][i])
+                is_done = true;
+        }
+        if (is_done)
+            continue;
+        done[r][c][x] = true;
+        //cout3(r, c, x);
+        REP(i, 0, 4) {
+            if (r+move_r[i] < 0 || r+move_r[i] > h-1 ||
+                c+move_c[i] < 0 || c+move_c[i] > w-1)
+                continue;
+            lli next_x = x;
+            if (s[r+move_r[i]][c+move_c[i]] == '#')
+                next_x++;
+
+            que.push(tup(r+move_r[i], c+move_c[i], next_x));
+        }
+    }
+    REP(i, 0, 3) {
+        if (done[gi][gj][i]) {
+            cout << "YES" << endl;
+            return 0;
+        }
+    }
+    cout << "NO" << endl;
 
 }
