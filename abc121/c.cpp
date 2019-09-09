79e69233aeed436631daf8650b6adb5410fbcde0
longtime1116 <longtime1116@gmail.com>
2019-09-09 11:00:34 +0900

    abc121 a b c d

---
+++ abc121/c.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// iterator
+#define REP(i,init, n) for(lli i=init;i<n;i++)
+#define REPE(i,init, n) for(lli i=init;i<=n;i++)
+#define REPIT(it,container) for(auto it = container.begin(); it != container.end(); it++)
+#define REPIT_R(it,container) for(auto it = container.rbegin(); it != container.rend(); it++)
+
+// input
+#define cin1(x)             cin >> x
+#define cin2(x, y)          cin >> x >> y
+#define cin3(x, y, z)       cin >> x >> y >> z
+#define ncin1(n, x)         REP(i, 0, n) {cin1(x[i]);}
+#define ncin2(n, x, y)      REP(i, 0, n) {cin2(x[i], y[i]);}
+#define ncin3(n, x, y, z)   REP(i, 0, n) {cin3(x[i], y[i], z[i]);}
+
+// output
+#define cout1(x)         cout << #x << ": " << x << endl;
+#define cout2(x, y)      cout << #x << ": " << x << ", " << #y << ": " << y << endl;
+#define cout3(x, y, z)   cout << #x << ": " << x << ", " << #y << ": " << y << ", " << #z << ": " << z << endl;;
+#define ncout1(n, x)     REP(i, 0, n) {cout << #x << "[" << i << "]: "<< x[i] << endl;}
+
+#define coutp(p)         cout << #p << ":" <<  " (" << p.first << ", " << p.second << ")" << endl;
+
+// sort
+#define sort_r(x, y)        sort(x, y, greater<lli>()); // 降順(5,4,3,,,)
+
+#define ARRAY_LENGTH(array) (sizeof(array) / sizeof(array[0]))
+
+typedef long long int lli;
+typedef pair<lli, lli> P;
+typedef tuple<lli, lli, lli> tup;
+
+class CompareDist
+{
+public:
+    bool operator()(P n1, P n2) {
+        return n1.first>n2.first;
+    }
+};
+
+int main() {
+    lli n, m;
+    cin2(n, m);
+    lli a[n];
+    lli b[n];
+    ncin2(n, a, b);
+    priority_queue< P, vector<P>, CompareDist > que;
+    REP(i, 0, n) {
+        que.push(P(a[i], b[i]));
+    }
+
+    lli ans = 0;
+    while (true) {
+        lli cost, hon;
+        tie(cost, hon) = que.top();
+        que.pop();
+
+        if (m > hon) {
+            m -= hon;
+            ans += cost * hon;
+        }
+        else {
+            ans += cost * m;
+            break;
+        }
+    }
+    cout << ans << endl;
+
+
+}
