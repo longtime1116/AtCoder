cbc60cd8c89c1bfa4fd5a0a4706a6ab41b298035
longtime1116 <longtime1116@gmail.com>
2019-09-25 00:09:33 +0900

    tdpc j tmp

---
+++ tdpc/j.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// iterator
+#define REP(i,from, to) for(lli i=from;i<to;i++)
+#define REPE(i,from, to) for(lli i=from;i<=to;i++)
+#define REP_R(i,from, to) for(lli i=from;i>to;i--)
+#define REPE_R(i,from, to) for(lli i=from;i>=to;i--)
+#define REPIT(it,container) for(auto it = container.begin(); it != container.end(); it++)
+#define REPIT_R(it,container) for(auto it = container.rbegin(); it != container.rend(); it++)
+
+// input
+#define cin1(x)             cin >> x
+#define cin2(x, y)          cin >> x >> y
+#define cin3(x, y, z)       cin >> x >> y >> z
+#define ncin1(n, x)         REP(i, 0, n) {cin1(x[i]);}
+#define ncin2(n, x, y)      REP(i, 0, n) {cin2(x[i], y[i]);}
+#define ncin3(n, x, y, z)   REP(i, 0, n) {cin3(x[i], y[i], z[i]);}
+
+// output
+#define cout1(x)         cout << #x << ": " << x << endl;
+#define cout2(x, y)      cout << #x << ": " << x << ", " << #y << ": " << y << endl;
+#define cout3(x, y, z)   cout << #x << ": " << x << ", " << #y << ": " << y << ", " << #z << ": " << z << endl;;
+#define ncout1(n, x)     REP(i, 0, n) {cout << #x << "[" << i << "]: "<< x[i] << endl;}
+
+#define coutp(p)         cout << #p << ":" <<  " (" << p.first << ", " << p.second << ")" << endl;
+
+// sort
+#define sort_r(x, y)        sort(x, y, greater<lli>()); // 降順(5,4,3,,,)
+
+#define ARRAY_LENGTH(array) (sizeof(array) / sizeof(array[0]))
+
+typedef long long int lli;
+typedef pair<lli, lli> P;
+typedef tuple<lli, lli, lli> tup;
+
+// dp[i] := i のときの期待値
+//          ただし、iは状態をbitで表す整数
+unsigned lli dp[2^17];
+int main() {
+    lli n;
+    cin1(n);
+    lli x[n];
+    ncin1(n,x);
+    REP(i,0,n) {
+        cin >> x[i+1];
+    }
+    sort(x, x+n);
+    // x
+    // 1,3,7,8,9,10
+
+    // 分割した上で一個ずつ処理する
+    {
+        // 影響のあるところだけを抜き出す
+        vector<lli> cur;
+        REP(i,0,n){
+        }
+        // その範囲で、すべての箇所から初めて一番期待値が低いやつ選ぶ
+    }
+    // 全部足す
+
+}
