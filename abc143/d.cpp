a924c64af3f9bfb5f9922e8e07bc7fa68b22656b
longtime1116 <longtime1116@gmail.com>
2019-10-19 23:56:11 +0900

    abc143

---
+++ abc143/d.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// iterator
+#define REP(i,from, to) for(lli i=from;i<to;i++)
+#define REPE(i,from, to) for(lli i=from;i<=to;i++)
+#define REP_R(i,from, to) for(lli i=from;i>to;i--)
+#define REPE_R(i,from, to) for(lli i=from;i>=to;i--)
+#define REPIT(it,container) for(auto it = container.begin(); it != container.end(); it++)
+#define REPIT_R(it,container) for(auto it = container.rbegin(); it != container.rend(); it++)
+
+// input
+#define cin1(x)             cin >> x
+#define cin2(x, y)          cin >> x >> y
+#define cin3(x, y, z)       cin >> x >> y >> z
+#define ncin1(n, x)         REP(i, 0, n) {cin1(x[i]);}
+#define ncin2(n, x, y)      REP(i, 0, n) {cin2(x[i], y[i]);}
+#define ncin3(n, x, y, z)   REP(i, 0, n) {cin3(x[i], y[i], z[i]);}
+
+// output
+#define cout1(x)         cout << #x << ": " << x << endl;
+#define cout2(x, y)      cout << #x << ": " << x << ", " << #y << ": " << y << endl;
+#define cout3(x, y, z)   cout << #x << ": " << x << ", " << #y << ": " << y << ", " << #z << ": " << z << endl;;
+#define ncout1(n, x)     REP(i, 0, n) {cout << #x << "[" << i << "]: "<< x[i] << endl;}
+
+#define coutp(p)         cout << #p << ":" <<  " (" << p.first << ", " << p.second << ")" << endl;
+
+// sort
+#define sort_r(x, y)        sort(x, y, greater<lli>()); // 降順(5,4,3,,,)
+
+#define ARRAY_LENGTH(array) (sizeof(array) / sizeof(array[0]))
+
+typedef long long int lli;
+typedef pair<lli, lli> P;
+typedef tuple<lli, lli, lli> tup;
+
+
+lli num[1001]; // i の長さ未満の本数の合計
+bool done[1001][1001];
+
+int main() {
+    lli n;
+    cin1(n);
+    lli l[n];
+    ncin1(n,l);
+
+    sort(l, l+n);
+
+    lli ans = 0;
+    REP(i,0,n) {
+        REP(j,i+1,n) {
+            // j+1 〜n の間で満たす個数
+            auto it_r = lower_bound(l, l+n, l[i]+l[j]);
+            if ((lli)(it_r-1 - &l[j]) <= 0)
+                continue;
+            ans += (lli)(it_r-1 - l-j);
+
+        }
+    }
+    cout << ans << endl;
+}
