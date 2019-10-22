1675e8abe4c57add5ae225e511738b35a9ea9ebb
longtime1116 <longtime1116@gmail.com>
2019-10-22 20:10:12 +0900

    update abc075 c

--- abc075/c.cpp
+++ abc075/c.cpp
@@ -33,27 +33,81 @@ using namespace std;
 typedef long long int lli;
 typedef pair<lli, lli> P;
 typedef tuple<lli, lli, lli> tup;
+typedef vector<lli> vlli;
 
-vector<lli> G[51];
-bool done[51][51];
 
-bool rec(lli origin, lli from, lli to) {
-    if (done[from][to])
-        return false;
-    if (to == origin)
-        return true;
+vlli G[51];
 
-    done[to][from] = done[from][to] = true;
-    REP(i,0,G[to].size()) {
-        if (G[to][i] == from)
+lli dfs(vector<P> &res, lli cur, lli &count, lli from, vlli &low, vlli &pre) {
+    pre[cur] = count;
+    low[cur] = pre[cur];
+    count++;
+
+    // low と pre が等しくないノードは、必ず閉路に含まれる。
+    // その閉路には、1つだけ low と pre が等しいノードが含まれている(閉路代表と呼ぶ)
+    // low と pre が等しい => 閉路を構成しないノード or 閉路代表
+    REP(i,0,G[cur].size()) {
+        lli to = G[cur][i];
+        // 親に返るのを阻止
+        if (from == to) {
             continue;
-        if (rec(origin, to, G[to][i])) {
-            return true;
+        }
+
+        if (pre[to] == -1) {
+            // 初訪問。次のnodeに行った場合の low を調べ、必要があれば更新
+            low[cur] = min(low[cur], dfs(res, to, count, cur, low, pre));
+            if (low[to] == pre[to]) {
+                // to から先で完結しており、cur と to は同一グループ内に存在しない
+                res.push_back(P(cur, to));
+            }
+        }
+        else {
+            // 閉路であることが確認された。
+            low[cur] = min(low[cur], low[to]);
         }
     }
-    return false;
 
+    return low[cur];
 }
+
+// n: number of nodes
+void bridges(vector<P> &res, lli n) {
+    vlli low(n+1, -1); // 最も小さい、到達時のカウンタ
+    vlli pre(n+1, -1); // 走査されたときのカウンタ(pre-order)
+    lli count = 1; // nodeを訪れるごとにインクリメントされていくカウンタ
+    dfs(res, 1, count, -1, low, pre);
+}
+
+// n: number of nodes
+lli closed_path_count(vector<P> &res, lli n) {
+    vlli low(n+1, -1); // 最も小さい、到達時のカウンタ
+    vlli pre(n+1, -1); // 走査されたときのカウンタ(pre-order)
+    lli count = 1; // nodeを訪れるごとにインクリメントされていくカウンタ
+    dfs(res, 1, count, -1, low, pre);
+
+    //REP(i,0,res.size()) {
+    //    coutp(res[i]);
+    //}
+    //REPE(i,1,n) {
+    //    cout2(i,low[i]);
+    //}
+    sort(low.begin(), low.end());
+    lli ret = 0;
+    REP(i,0,res.size()) {
+        auto it1 = lower_bound(low.begin(), low.end(), res[i].second);
+        auto it2 = upper_bound(low.begin(), low.end(), res[i].second);
+
+        if ((it2 - it1) > 1)
+            ret++;
+    }
+    // 開始地点はresに含まれないので個別にチェック
+    auto it1 = lower_bound(low.begin(), low.end(), 1);
+    auto it2 = upper_bound(low.begin(), low.end(), 1);
+    if ((it2 - it1) > 1)
+        ret++;
+    return ret;
+}
+
 int main() {
     lli n,m;
     cin2(n,m);
@@ -63,22 +117,34 @@ int main() {
         G[a].push_back(b);
         G[b].push_back(a);
     }
-    lli ans = 0;
-    REPE(i,1,n) {
-        REP(j,0,G[i].size()) {
-            REPE(k,1,n) {
-                REPE(l,1,n) {
-                    done[l][k] = done[k][l] = false;
-                }
-            }
-
-            if (i > G[i][j])
-                continue;
-            if (rec(i, i,G[i][j])) {
-                //cout2(i,G[i][j])
-                ans++;
-            }
-        }
-    }
-    cout << m - ans << endl;
+    vector<P> res;
+    bridges(res, n);
+    cout << res.size() << endl;
+    //vector<P> res2;
+    //cout << closed_path_count(res2,n) << endl;
 }
+
+
+// 入力例
+// 8 9 1 2 2 3 3 4 4 1 5 6 6 7 7 8 8 5 2 5
+//
+// o-o o-o
+// | | | |
+// o-o-o-o
+//
+// 8 10 1 2 2 3 3 4 4 1 5 6 6 7 7 8 8 5 2 5 3 8
+//
+// o-o-o-o
+// | | | |
+// o-o-o-o
+//
+// 9 10 1 2 2 3 2 4 4 5 5 6 5 7 7 8 8 9 4 8 2 9
+//
+//   o-o-o
+//   | | |
+// o-o-o-o-o
+//   |
+//   o
+//
+
+// 参考: http://nupioca.hatenadiary.jp/entry/2013/11/03/200006
