b8899071735a10f666ca286f5bcd0124eb16bf20
shumo.tokinaga <longtime1116@gmail.com>
2019-10-24 14:55:52 +0900

    abc084 c

---
+++ abc084/c.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// iterator
+#define REP(i,from, to) for(lli i=from;i<to;i++)
+#define REPE(i,from, to) for(lli i=from;i<=to;i++)
+#define REP_R(i,from, to) for(lli i=from;i>to;i--)
+#define REPE_R(i,from, to) for(lli i=from;i>=to;i--)
+#define REPIT(it,container) for(auto it = container.begin(); it != container.end(); it++)
+#define REPIT_R(it,container) for(auto it = container.rbegin(); it != container.rend(); it++)
+
+// input
+#define cin1(x)             cin >> x
+#define cin2(x, y)          cin >> x >> y
+#define cin3(x, y, z)       cin >> x >> y >> z
+#define ncin1(n, x)         REP(i, 0, n) {cin1(x[i]);}
+#define ncin2(n, x, y)      REP(i, 0, n) {cin2(x[i], y[i]);}
+#define ncin3(n, x, y, z)   REP(i, 0, n) {cin3(x[i], y[i], z[i]);}
+
+// output
+#define cout1(x)         cout << #x << ": " << x << endl;
+#define cout2(x, y)      cout << #x << ": " << x << ", " << #y << ": " << y << endl;
+#define cout3(x, y, z)   cout << #x << ": " << x << ", " << #y << ": " << y << ", " << #z << ": " << z << endl;;
+#define ncout1(n, x)     REP(i, 0, n) {cout << #x << "[" << i << "]: "<< x[i] << endl;}
+
+#define coutp(p)         cout << #p << ":" <<  " (" << p.first << ", " << p.second << ")" << endl;
+
+// sort
+#define sort_r(x, y)        sort(x, y, greater<lli>()); // 降順(5,4,3,,,)
+
+#define ARRAY_LENGTH(array) (sizeof(array) / sizeof(array[0]))
+
+typedef long long int lli;
+typedef pair<lli, lli> P;
+typedef tuple<lli, lli, lli> tup;
+typedef vector<lli> vlli;
+
+int main() {
+    lli n;
+    cin1(n);
+    lli c[n-1];
+    lli s[n-1];
+    lli f[n-1];
+    ncin3(n-1,c,s,f);
+
+    vlli x;
+    REP(i,0,n-1) {
+        lli cur = 0;
+        REP(j,i,n-1) {
+            if (cur < s[j])
+                cur = s[j];
+            else {
+                // s[j] + f[j] * n が cur を超える最小の n
+                lli rest = cur - s[j];
+                if (rest % f[j] != 0)
+                    cur = s[j] + ((rest/f[j]) + 1) * f[j];
+            }
+            cur += c[j];
+        }
+        x.push_back(cur);
+    }
+    x.push_back(0);
+    REPIT(it, x) {
+        cout << *it << endl;
+    }
+}
