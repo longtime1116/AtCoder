6d39defdf3b3fcc3f5f57e49d1c8e0035c209eb4
shumo.tokinaga <longtime1116@gmail.com>
2019-11-05 12:28:36 +0900

    update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,8 +1,17 @@
 mkdir $1
 if [ $? -eq 0 ]; then
-  cp ./template.cpp ./$1/a.cpp
-  cp ./template.cpp ./$1/b.cpp
-  cp ./template.cpp ./$1/c.cpp
-  cp ./template.cpp ./$1/d.cpp
-  cd $1
+  if [ $# -eq 1 ]; then
+    cp ./template.cpp ./$1/a.cpp
+    cp ./template.cpp ./$1/b.cpp
+    cp ./template.cpp ./$1/c.cpp
+    cp ./template.cpp ./$1/d.cpp
+    cp ./template.cpp ./$1/e.cpp
+    cp ./template.cpp ./$1/f.cpp
+    cd $1
+  elif [ $# -eq 2 ]; then
+    cp ./template.cpp ./$1/$2.cpp
+    cd $1
+  else
+    rm -r $1
+  fi
 fi
