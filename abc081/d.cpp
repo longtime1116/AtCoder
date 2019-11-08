e87ac9ec39c058e9d6b065a1da94301aa9f1eb76
shumo.tokinaga <longtime1116@gmail.com>
2019-11-08 19:16:23 +0900

    abc081 d

---
+++ abc081/d.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// iterator
+#define REP(i,from, to) for(lli i=from;i<to;i++)
+#define REPE(i,from, to) for(lli i=from;i<=to;i++)
+#define REP_R(i,from, to) for(lli i=from;i>to;i--)
+#define REPE_R(i,from, to) for(lli i=from;i>=to;i--)
+#define REPIT(it,container) for(auto it = container.begin(); it != container.end(); it++)
+#define REPIT_R(it,container) for(auto it = container.rbegin(); it != container.rend(); it++)
+
+// input
+#define cin1(x)             cin >> x
+#define cin2(x, y)          cin >> x >> y
+#define cin3(x, y, z)       cin >> x >> y >> z
+#define ncin1(n, x)         REP(i, 0, n) {cin1(x[i]);}
+#define ncin2(n, x, y)      REP(i, 0, n) {cin2(x[i], y[i]);}
+#define ncin3(n, x, y, z)   REP(i, 0, n) {cin3(x[i], y[i], z[i]);}
+
+// output
+#define cout1(x)         cout << #x << ": " << x << endl;
+#define cout2(x, y)      cout << #x << ": " << x << ", " << #y << ": " << y << endl;
+#define cout3(x, y, z)   cout << #x << ": " << x << ", " << #y << ": " << y << ", " << #z << ": " << z << endl;;
+#define ncout1(n, x)     REP(i, 0, n) {cout << #x << "[" << i << "]: "<< x[i] << endl;}
+
+#define coutp(p)         cout << #p << ":" <<  " (" << p.first << ", " << p.second << ")" << endl;
+
+// sort
+#define sort_r(x, y)        sort(x, y, greater<lli>()); // 降順(5,4,3,,,)
+
+#define ARRAY_LENGTH(array) (sizeof(array) / sizeof(array[0]))
+
+typedef long long int lli;
+typedef pair<lli, lli> P;
+typedef tuple<lli, lli, lli> tup;
+typedef vector<lli> vlli;
+
+int main() {
+    lli n;
+    cin1(n);
+}
+
+
+// 1 8 2 3 1
+// 1 8 10 3 1
+// 1 8 10 13 1
+// 1 8 10 13 14
+//
+// 2 1 3 2 1
+// 2 3 3 2 1
+// 2 3 6 2 1
+// 2 3 6 8 1
+// 2 3 6 8 9
+//
+//
+// 絶対値が一番大きいのは正 => 正にする(高々n-1回)
+// -2 5 -1
+// 3 5 -1
+// 3 5 4
+// 3 5 9
+//
+//
+//
+// -50 -1 -100 5 -200
+// -50 -1 -100 -195 -200
+// -546 -496 -495 -395 -200
+// まず正負をそろえ、次に正なら左から、負なら右からやっていく
