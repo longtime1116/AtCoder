e9f3e6d7ffdcc232fa11d1d1e602a12d4cabdf5b
shumo.tokinaga <longtime1116@gmail.com>
2019-11-18 16:52:16 +0900

    update template.cpp

--- template.cpp
+++ template.cpp
@@ -13,9 +13,14 @@ using namespace std;
 #define cin1(x)             cin >> x
 #define cin2(x, y)          cin >> x >> y
 #define cin3(x, y, z)       cin >> x >> y >> z
-#define ncin1(n, x)         REP(i, 0, n) {cin1(x[i]);}
-#define ncin2(n, x, y)      REP(i, 0, n) {cin2(x[i], y[i]);}
-#define ncin3(n, x, y, z)   REP(i, 0, n) {cin3(x[i], y[i], z[i]);}
+#define ncin1(n, x)         REP(i, 0, n) { cin1(x[i]); }
+#define ncin2(n, x, y)      REP(i, 0, n) { cin2(x[i], y[i]); }
+#define ncin3(n, x, y, z)   REP(i, 0, n) { cin3(x[i], y[i], z[i]); }
+#define ncinp(n, p)         REP(i, 0, n) { lli p1,p2; cin >> p1 >> p2; p[i] = P(p1, p2); }
+#define ncin1_1(n, x)       REPE(i, 1, n) { cin1(x[i]); }
+#define ncin2_1(n, x, y)    REPE(i, 1, n) { cin2(x[i], y[i]); }
+#define ncin3_1(n, x, y, z) REPE(i, 1, n) { cin3(x[i], y[i], z[i]); }
+#define ncinp_1(n, p)       REPE(i, 1, n) { lli p1,p2; cin >> p1 >> p2; p[i] = P(p1, p2); }
 
 // output
 #define cout1(x)         cout << #x << ": " << x << endl;
