36daacc94e923e2f81609d12a9cad0ea1df85a60
longtime1116 <longtime1116@gmail.com>
2019-11-26 23:31:53 +0900

    abc091 c

---
+++ abc091/c.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// iterator
+#define REP(i,from, to) for(lli i=from;i<to;i++)
+#define REPE(i,from, to) for(lli i=from;i<=to;i++)
+#define REP_R(i,from, to) for(lli i=from;i>to;i--)
+#define REPE_R(i,from, to) for(lli i=from;i>=to;i--)
+#define REPIT(it,container) for(auto it = container.begin(); it != container.end(); it++)
+#define REPIT_R(it,container) for(auto it = container.rbegin(); it != container.rend(); it++)
+
+// input
+#define cin1(x)             cin >> x
+#define cin2(x, y)          cin >> x >> y
+#define cin3(x, y, z)       cin >> x >> y >> z
+#define ncin1(n, x)         REP(i, 0, n) { cin1(x[i]); }
+#define ncin2(n, x, y)      REP(i, 0, n) { cin2(x[i], y[i]); }
+#define ncin3(n, x, y, z)   REP(i, 0, n) { cin3(x[i], y[i], z[i]); }
+#define ncinp(n, p)         REP(i, 0, n) { lli p1,p2; cin >> p1 >> p2; p[i] = P(p1, p2); }
+#define ncin1_1(n, x)       REPE(i, 1, n) { cin1(x[i]); }
+#define ncin2_1(n, x, y)    REPE(i, 1, n) { cin2(x[i], y[i]); }
+#define ncin3_1(n, x, y, z) REPE(i, 1, n) { cin3(x[i], y[i], z[i]); }
+#define ncinp_1(n, p)       REPE(i, 1, n) { lli p1,p2; cin >> p1 >> p2; p[i] = P(p1, p2); }
+
+// output
+#define cout1(x)         cout << #x << ": " << x << endl;
+#define cout2(x, y)      cout << #x << ": " << x << ", " << #y << ": " << y << endl;
+#define cout3(x, y, z)   cout << #x << ": " << x << ", " << #y << ": " << y << ", " << #z << ": " << z << endl;;
+#define ncout1(n, x)     REP(i, 0, n) {cout << #x << "[" << i << "]: "<< x[i] << endl;}
+
+#define coutp(p)         cout << #p << ":" <<  " (" << p.first << ", " << p.second << ")" << endl;
+
+// sort
+#define sort_r(x, y)        sort(x, y, greater<lli>()); // 降順(5,4,3,,,)
+
+#define ARRAY_LENGTH(array) (sizeof(array) / sizeof(array[0]))
+
+typedef long long int lli;
+typedef pair<lli, lli> P;
+typedef tuple<lli, lli, lli> tup;
+typedef vector<lli> vlli;
+
+int main() {
+    lli n;
+    cin1(n);
+    P a[n];
+    P b[n];
+    REP(i,0,n) {
+        lli x, y;
+        cin >> x >> y;
+        a[i] = P(x,y);
+    }
+    REP(i,0,n) {
+        lli x, y;
+        cin >> x >> y;
+        b[i] = P(x,y);
+    }
+
+
+
+    sort(b, b+n);
+    bool is_done[n] = {false};
+    lli ans = 0;
+    REP(i,0,n) {
+        // b[i] よりも左下にある a を探し、yが最も大きいものを選んでペアにする
+        lli max_y = -1;
+        lli ind = -1;
+        REP(j,0,n) {
+            if (is_done[j])
+                continue;
+            // a[j] は b[i] の左下にある？
+            if (a[j].first < b[i].first && a[j].second < b[i].second) {
+                if (max_y < a[j].second) {
+                    max_y = a[j].second;
+                    ind = j;
+                }
+            }
+        }
+        if (max_y != -1) {
+            ans++;
+            is_done[ind] = true;
+        }
+    }
+    cout << ans << endl;
+
+}
