7ed7853e3bb2b5c85ba1769f040beff1d2fdc3e2
longtime1116 <longtime1116@gmail.com>
2019-12-01 20:50:12 +0900

    abc098 d tmp

---
+++ abc098/d.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// iterator
+#define REP(i,from, to) for(lli i=from;i<to;i++)
+#define REPE(i,from, to) for(lli i=from;i<=to;i++)
+#define REP_R(i,from, to) for(lli i=from;i>to;i--)
+#define REPE_R(i,from, to) for(lli i=from;i>=to;i--)
+#define REPIT(it,container) for(auto it = container.begin(); it != container.end(); it++)
+#define REPIT_R(it,container) for(auto it = container.rbegin(); it != container.rend(); it++)
+
+// input
+#define cin1(x)             cin >> x
+#define cin2(x, y)          cin >> x >> y
+#define cin3(x, y, z)       cin >> x >> y >> z
+#define ncin1(n, x)         REP(i, 0, n) { cin1(x[i]); }
+#define ncin2(n, x, y)      REP(i, 0, n) { cin2(x[i], y[i]); }
+#define ncin3(n, x, y, z)   REP(i, 0, n) { cin3(x[i], y[i], z[i]); }
+#define ncinp(n, p)         REP(i, 0, n) { lli p1,p2; cin >> p1 >> p2; p[i] = P(p1, p2); }
+#define ncin1_1(n, x)       REPE(i, 1, n) { cin1(x[i]); }
+#define ncin2_1(n, x, y)    REPE(i, 1, n) { cin2(x[i], y[i]); }
+#define ncin3_1(n, x, y, z) REPE(i, 1, n) { cin3(x[i], y[i], z[i]); }
+#define ncinp_1(n, p)       REPE(i, 1, n) { lli p1,p2; cin >> p1 >> p2; p[i] = P(p1, p2); }
+
+// output
+#define cout1(x)         cout << #x << ": " << x << endl;
+#define cout2(x, y)      cout << #x << ": " << x << ", " << #y << ": " << y << endl;
+#define cout3(x, y, z)   cout << #x << ": " << x << ", " << #y << ": " << y << ", " << #z << ": " << z << endl;;
+#define ncout1(n, x)     REP(i, 0, n) {cout << #x << "[" << i << "]: "<< x[i] << endl;}
+
+#define coutp(p)         cout << #p << ":" <<  " (" << p.first << ", " << p.second << ")" << endl;
+
+// sort
+#define sort_r(x, y)        sort(x, y, greater<lli>()); // 降順(5,4,3,,,)
+
+#define ARRAY_LENGTH(array) (sizeof(array) / sizeof(array[0]))
+
+typedef long long int lli;
+typedef pair<lli, lli> P;
+typedef tuple<lli, lli, lli> tup;
+typedef vector<lli> vlli;
+
+lli factorial[200001];
+void calc_factorial() {
+    REP(i,1,200001) {
+        factorial[i] = factorial[i-1] + i;
+    }
+}
+
+int main() {
+    lli n;
+    cin1(n);
+    lli a[n];
+    ncin1(n,a);
+
+    //calc_factorial();
+    lli cur_xor = 0;
+    lli cur_sum = 0;
+    lli ans = 0;
+    lli l = 0;
+    lli r = 0;
+    for (r = 0; r < n; r++) {
+        //
+
+    }
+}
+
+
+// 5 5 2 4 1 1
+//
+// 1 0 1
+// 0 1 0
+// 1 0 0
+// 0 0 1
+// 0 0 1
+//
+// => 9
+
+
+
+// 6 8 1 2 9 4 4
+//
+// 1 0 0 0
+// 0 0 0 1
+// 0 0 1 0
+// 1 0 0 1
+// 0 1 0 0
+// 0 1 0 0
+//
+// => 12
